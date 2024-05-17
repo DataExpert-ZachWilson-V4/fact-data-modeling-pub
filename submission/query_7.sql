348f9ae34e890ac06ca73577a0a86ad5f2caab1d
Juanse Gomez <juan.gomez@dowjones.com>
2024-05-17 01:12:23 -0500

    Improved partitioning

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -6,8 +6,8 @@ CREATE OR REPLACE TABLE jsgomez14.host_activity_reduced
     month_start VARCHAR
 ) WITH (
     format='PARQUET', --specify the format of the table.
-    partitioning= ARRAY['month_start']
-    -- Specify the partitioning column.
+    partitioning = ARRAY['metric_name', 'month_start']
+    -- Specify the partitioning columns.
     -- To take advantage of Parquet's run length encoding 
     -- and compress repetitive data to save disk space.
 )
