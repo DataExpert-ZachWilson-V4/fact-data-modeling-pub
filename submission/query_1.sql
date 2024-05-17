36b0eb81ab0e25cd60e00d1ee8cabb996e11d6af
jrsarrat <151387051+jrsarrat@users.noreply.github.com>
2024-05-17 17:52:53 -0400

    Update query_1.sql

---
+++ submission/query_1.sql
@@ -0,0 +1,35 @@
+CREATE TABLE jrsarrat.fct_nba_game_details (
+  game_id BIGINT,
+  team_id BIGINT,
+  player_id BIGINT,
+  dim_team_abbreviation VARCHAR,
+  dim_player_name VARCHAR,
+  dim_start_position VARCHAR,
+  dim_did_not_dress BOOLEAN,
+  dim_not_with_team BOOLEAN,
+  m_seconds_played INTEGER,
+  m_field_goals_made DOUBLE,
+  m_field_goals_attempted DOUBLE,
+  m_3_pointers_made DOUBLE,
+  m_3_pointers_attempted DOUBLE,
+  m_free_throws_made DOUBLE,
+  m_free_throws_attempted DOUBLE,
+  m_offensive_rebounds DOUBLE,
+  m_defensive_rebounds DOUBLE,
+  m_rebounds DOUBLE,
+  m_assists DOUBLE,
+  m_steals DOUBLE,
+  m_blocks DOUBLE,
+  m_turnovers DOUBLE,
+  m_personal_fouls DOUBLE,
+  m_points DOUBLE,
+  m_plus_minus DOUBLE,
+  game_date DATE,
+  season INTEGER,
+  team_did_win BOOLEAN
+)
+WITH
+  (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['season']
+  )
