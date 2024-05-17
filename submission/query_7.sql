d2f6549d09566b4f287a38cd554ca73f7af16fc2
Luis Coelho <luis.coelho@burda-forward.de>
2024-05-17 19:38:57 +0200

    Use array of ints

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -3,7 +3,7 @@
 CREATE OR REPLACE TABLE luiscoelho37431.host_activity_reduced (
     host VARCHAR, -- The name of the host
     metric_name VARCHAR, -- The name of the metric
-    metric_array ARRAY<INT64>, -- An array of metric values
+    metric_array ARRAY(INTEGER), -- An array of metric values
     month_start VARCHAR -- The starting month of the data
 ) WITH (
     format = 'PARQUET', -- The file format for storing the table data
