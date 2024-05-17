b5148b792d6ca1dd4f41c3966e9f33e038d613b3
Akshay Jain <exclusivearj@gmail.com>
2024-05-17 16:03:05 -0700

    Test commit

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user_devices_cumulated ( -- CTE to hold cumulative user activity 
     user_id BIGINT,
     browser_type VARCHAR,
-    dates_active ARRAY(DATE)
+    dates_active ARRAY(DATE) -- datelist datelist that tracks how many times a user has been active with a given
     date DATE
 )
