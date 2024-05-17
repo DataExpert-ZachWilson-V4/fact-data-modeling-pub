79f08c394ba735160e9d539d927080cd30425e00
jrsarrat <151387051+jrsarrat@users.noreply.github.com>
2024-05-17 19:04:04 -0400

    Update query_2.sql

---
+++ submission/query_2.sql
@@ -0,0 +1,8 @@
+CREATE TABLE jrsarrat.user_devices_cumulated (
+  user_id BIGINT,
+  browser_type varchar,
+  dates_active ARRAY(DATE),
+  date DATE
+)
+WITH
+  (FORMAT = 'PARQUET', partitioning = ARRAY['date'])
