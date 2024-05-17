a7dc5a853bcaa5db72bb6a972d9db535743d48fb
Akshay Jain <exclusivearj@gmail.com>
2024-05-17 15:43:23 -0700

    Current state

---
+++ submission/query_8.sql
@@ -0,0 +1,24 @@
+INSERT INTO akshayjainytl54781.host_activity_reduced
+
+WITH yesterday AS (
+    SELECT * FROM
+    akshayjainytl54781.host_activity_reduced
+    WHERE month_start = '2023-01-01'
+),
+
+today AS (
+    SELECT * FROM akshayjainytl54781.daily_web_metrics -- TODO: Remove schema name
+    WHERE date = DATE '2023-01-02'
+)
+
+SELECT 
+    COALESCE(t.host, y.host) host,
+    COALESCE(t.metric_name, y.metric_name) metric_name,
+    COALESCE(y.metric_array, REPEAT(
+    null,
+    CAST(DATE_DIFF('day', DATE('2023-01-01'),
+    t.date) AS INTEGER))) || ARRAY[t.metric_value] AS metric_array,
+    '2023-01-01' AS month_start
+FROM
+    today t
+FULL OUTER JOIN yesterday y ON (t.host = y.host) AND (t.metric_name = y.metric_name)
