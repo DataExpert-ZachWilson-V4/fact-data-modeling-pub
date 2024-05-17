a55a4216aea43b643aa5200345d3812d7f93be0e
bgar8 <92387983+bgar8@users.noreply.github.com>
2024-05-16 20:42:24 -0700

    Update query_8.sql

---
+++ submission/query_8.sql
@@ -0,0 +1,20 @@
+INSERT INTO bgar.host_activity_reduced
+WITH yesterday AS (
+SELECT * 
+FROM bgar.host_activity_reduced
+WHERE month_start = DATE('2023-08-01')
+),
+today AS (
+  SELECT *
+  FROM bgar.daily_web_metrics
+  WHERE date = DATE('2023-08-01')
+)
+SELECT
+  COALESCE(t.user_id, y.user_id) as user_id,
+  COALESCE(t.metric_name, y.metric_name) as metric_name,
+  COALESCE(y.metric_array, REPEAT(NULL, CAST(DATE_DIFF('day', DATE('2023-08-01'), t.date) AS INTEGER))) || ARRAY[t.metric_value] as metric_array,
+  '2023-08-01' as month_start
+FROM today t 
+FULL OUTER JOIN yesterday y 
+ON t.user_id = y.user_id
+AND t.metric_name = y.metric_name
