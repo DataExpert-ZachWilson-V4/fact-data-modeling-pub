def320a8fe9fd0e939aaca5df42986db0371b488
Akshay Jain <exclusivearj@gmail.com>
2024-05-17 16:27:45 -0700

    Small comments

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE TABLE akshayjainytl54781.host_activity_reduced (
+CREATE OR REPLACE TABLE akshayjainytl54781.host_activity_reduced ( -- CTE to hold monthly host activity
     host VARCHAR,
     metric_name VARCHAR,
-    metric_array ARRAY(INTEGER),
-    month_start VARCHAR
+    metric_array ARRAY(INTEGER), -- Date array for each day of the month
+    month_start VARCHAR -- Will always be the 1st day of a month
 ) WITH (
     format = 'PARQUET',
     partitioning = ARRAY['metric_name', 'month_start']
