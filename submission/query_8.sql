7e5abf48c49843eca64111d30102bb2301344f67
Akshay Jain <exclusivearj@gmail.com>
2024-05-17 21:56:00 -0700

    query 8 working

--- submission/query_8.sql
+++ submission/query_8.sql
@@ -7,13 +7,15 @@ WITH yesterday AS (
 ),
 
 today AS (
-    SELECT * FROM akshayjainytl54781.daily_web_metrics -- TODO: Remove schema name
+    SELECT * FROM daily_web_metrics
     WHERE date = DATE '2023-01-02'
 )
 
 SELECT 
     COALESCE(t.host, y.host) host,
     COALESCE(t.metric_name, y.metric_name) metric_name,
+    -- The `REPEAT` function is pretty cool. It helps repeat an element 'n' number of times
+    -- In this case, we fill the array with existing values + NULLs for missing dates - no dates in the month would be missing now
     COALESCE(y.metric_array, REPEAT(
     null,
     CAST(DATE_DIFF('day', DATE('2023-01-01'),
