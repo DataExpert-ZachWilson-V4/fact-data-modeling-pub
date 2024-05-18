fa279f5c4c5d2f8dce2881f6da8b98815c8af77e
mposada97 <85133902+mposada97@users.noreply.github.com>
2024-05-18 15:26:28 -0500

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -5,7 +5,7 @@ WITH
     FROM
       mposada.user_devices_cumulated
     WHERE
-      DATE = DATE('2023-01-04')
+      DATE = DATE('2023-01-07') -- Adjusted date to match prompt
   ),
   date_list_int AS (
     SELECT
@@ -14,21 +14,20 @@ WITH
       CAST(
         SUM(
           CASE
-            WHEN CONTAINS(dates_active, sequence_date) THEN POW(2, 30 - DATE_DIFF('day', sequence_date, DATE))
+            WHEN CONTAINS(dates_active, sequence_date) THEN POW(2, 31 - DATE_DIFF('day', sequence_date, DATE)) -- calculates the power of 2
             ELSE 0
           END
         ) AS BIGINT
       ) AS history_int
     FROM
       today
-      CROSS JOIN UNNEST (SEQUENCE(DATE('2023-01-02'), DATE('2023-01-04'))) AS t (sequence_date)
+      CROSS JOIN UNNEST (SEQUENCE(DATE('2023-01-01'), DATE('2023-01-07'))) AS t (sequence_date) -- creates a sequence of dates in that range and by cross join unnest it creates one row per date in the sequence
     GROUP BY
       user_id, 
       browser_type
   )
 SELECT
   *,
-  TO_BASE(history_int, 2) 
+  TO_BASE(history_int, 2) AS history_base_2
 FROM
   date_list_int 
-  
