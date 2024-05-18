f2836a69e190152d0b321ef227ae3abcf077a73e
Nitin Tharwani <tharwaninitin182@gmail.com>
2024-05-19 04:30:02 +0530

    Update query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -3,8 +3,8 @@
 CREATE OR REPLACE TABLE tharwaninitin.user_devices_cumulated (
   user_id BIGINT,
   browser_type VARCHAR,
-  dates_active ARRAY(DATE),		-- ARRAY of dates active
-  date DATE		                -- Current load date
+  dates_active ARRAY(DATE),		-- array of dates active
+  date DATE		                -- current load date
 ) WITH (
   FORMAT = 'PARQUET',
   PARTITIONING = ARRAY['date']
