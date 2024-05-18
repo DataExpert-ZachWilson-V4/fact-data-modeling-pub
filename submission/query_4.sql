580b29e6bfb52160576c850a831fd0f7f32301f9
jrsarrat <151387051+jrsarrat@users.noreply.github.com>
2024-05-17 22:38:41 -0400

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -6,21 +6,27 @@ WITH
       jrsarrat.user_devices_cumulated
     WHERE
       date = DATE('2023-01-08')
-  )
+  ),
+  date_list_int AS (
+    SELECT
+      user_id,
+      CAST(
+        SUM(
+          CASE
+            WHEN CONTAINS(dates_active, sequence_date) THEN POW(2, 31 - DATE_DIFF('day', sequence_date, DATE))
+            ELSE 0
+          END
+        ) AS BIGINT
+      ) AS history_int
+  FROM
+    today
+    CROSS JOIN UNNEST (SEQUENCE(DATE('2023-01-02'), DATE('2023-01-08'))) AS t (sequence_date)
+  GROUP BY
+  user_id,
+  browser_id
+)
 SELECT
-  user_id,
-  browser_id
-  CAST(
-    SUM(
-      CASE
-        WHEN CONTAINS(dates_active, sequence_date) THEN POW(2, 32 - DATE_DIFF('day', sequence_date, DATE))
-        ELSE 0
-      END
-    ) AS BIGINT
-  )
+  *,
+  TO_BASE(history_int, 2) AS history_in_binary
 FROM
-  today
-  CROSS JOIN UNNEST (SEQUENCE(DATE('2023-01-01'), DATE('2023-01-08'))) AS t (sequence_date)
-GROUP BY
-  user_id,
-  browser_id
+  date_list_int
