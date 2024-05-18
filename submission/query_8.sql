a9de03bd4176737fd0ea1af0788881b5639dd382
jessicadesilva <jdesilva1@csustan.edu>
2024-05-17 17:23:04 -0700

    query 8 draft

---
+++ submission/query_8.sql
@@ -0,0 +1,39 @@
+INSERT INTO
+    jessicadesilva.host_activity_reduced
+WITH
+    yesterday AS (
+        SELECT
+            *
+        FROM
+            jessicadesilva.host_activity_reduced
+        WHERE
+            month_start = DATE('2023-08-01')
+    ),
+    today AS (
+        SELECT
+            *
+        FROM
+            bootcamp.daily_web_metrics
+        WHERE
+            date = DATE('2023-08-03')
+    )
+SELECT
+    COALESCE(t.host, y.host) AS host,
+    COALESCE(t.metric_name, y.metric_name) AS metric_name,
+    CONCAT(
+        COALESCE(
+            y.metric_array,
+            REPEAT(
+                null,
+                CAST(
+                    DATE_DIFF('day', DATE('2023-08-01'), t.date) AS INTEGER
+                )
+            )
+        ),
+        ARRAY[t.metric_value]
+    ) AS metric_array,
+    DATE('2023-08-01') AS month_start
+FROM
+    today t
+    FULL OUTER JOIN yesterday y ON t.user_id = y.user_id
+    AND t.metric_name = y.metric_name
