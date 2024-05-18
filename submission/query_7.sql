222025c5231d23c151c5b3adb99712df5ed9de1c
mposada97 <85133902+mposada97@users.noreply.github.com>
2024-05-18 16:22:29 -0500

    Update query_7.sql

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -1,7 +1,7 @@
-CREATE OR REPLACE TABLE host_activity_reduced (
-  host varchar
-  metric_name varchar
-  metric_array array(integer)
+CREATE OR REPLACE TABLE mposada.host_activity_reduced (
+  host varchar,
+  metric_name varchar,
+  metric_array array(integer),
   month_start varchar
 ) WITH (
     format = 'PARQUET',
