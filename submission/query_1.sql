32e37802f750ec11e58359480c9290ca43fa4a6f
Raj <raj@Rajs-MacBook-Air.local>
2024-05-18 17:31:11 -0400

    hw2 first 3 queries

---
+++ submission/query_1.sql
@@ -0,0 +1,14 @@
+-- CTE to identify and rank duplicate rows based on game_id, team_id, and player_id
+WITH unique_games AS (
+    SELECT
+        *,
+        -- Assign a unique row number to each row within the partition
+        ROW_NUMBER() OVER (PARTITION BY game_id, team_id, player_id ORDER BY game_id) AS row_num
+    FROM
+        nba_game_details
+)
+
+-- Select only the first occurrence of each combination of game_id, team_id, and player_id
+SELECT *
+FROM unique_games
+WHERE row_num = 1
---
+++ submission/query_2.sql
@@ -0,0 +1,21 @@
+-- Create or replace the table user_devices_cumulated
+CREATE OR REPLACE TABLE raj.user_devices_cumulated (
+    -- The user ID
+    user_id BIGINT,
+    
+    -- The type of browser used by the user
+    browser_type VARCHAR,
+    
+    -- Array of dates when the user was active
+    dates_active ARRAY(DATE),
+    
+    -- The date of the activity (used for partitioning)
+    date DATE
+)
+WITH (
+    -- Specify the file format for the table
+    FORMAT = 'PARQUET',
+    
+    -- Partition the table by the date column for performance optimization
+    partitioning = ARRAY['date']
+)
