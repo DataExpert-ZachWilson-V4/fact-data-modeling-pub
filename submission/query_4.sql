690a27e96b8936968a302bf7e64c4d5d2a5af9da
Carlos Laguna <carlos.alr88@gmail.com>
2024-05-17 20:48:15 -0700

    query 4

---
+++ submission/query_4.sql
@@ -0,0 +1,19 @@
+WITH 
+today AS (
+    SELECT * FROM carloslaguna21592.user_devices_cumulated
+    WHERE date = DATE('2023-01-05')
+)
+
+SELECT
+    user_id,
+    CAST( SUM(
+        CASE WHEN 
+            CONTAINS(dates_active, sequence_date)
+            THEN POW(2, 32 - DATE_DIFF('day', sequence_date , date))
+            ELSE 0
+        END
+    ) AS BIGINT)
+    
+FROM today
+CROSS JOIN UNNEST(SEQUENCE(DATE('2023-01-01'), DATE('2023-01-05'))) AS t(sequence_date)
+GROUP BY user_id
