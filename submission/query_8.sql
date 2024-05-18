b64ffc266b4cb70a8fec064e81c4fd358c4d14b9
Eric Barroca <barrocaeric@gmail.com>
2024-05-17 21:44:02 -0300

    Reduced Host Fact Array

---
+++ submission/query_8.sql
@@ -0,0 +1,36 @@
+INSERT INTO
+    barrocaeric.host_activity_reduced
+WITH
+    yesterday AS (
+        SELECT
+            *
+        FROM
+            barrocaeric.host_activity_reduced
+        WHERE
+            month_start = '2023-08-01'
+    ),
+    today AS (
+        SELECT
+            *
+        FROM
+            barrocaeric.daily_web_metrics
+        WHERE
+            date = DATE('2023-08-01')
+    )
+SELECT
+    COALESCE(t.host, y.host) as host,
+    COALESCE(t.metric_name, y.metric_name),
+    COALESCE(
+        y.metric_array,
+        REPEAT(
+            null,
+            CAST(
+                DATE_DIFF('day', DATE('2023-08-01'), t.date) AS INTEGER
+            )
+        )
+    ) || ARRAY[t.metric_value] as metric_array,
+    '2023-08-01' as month_start
+FROM
+    today t
+    FULL OUTER JOIN yesterday y ON t.host = y.host
+    AND t.metric_name = y.metric_name
