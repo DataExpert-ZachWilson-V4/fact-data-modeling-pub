f832f0ef44bc1ed42c94bb001c78458e30abc7a9
videetm <vids107@gmail.com>
2024-05-18 11:25:19 -0400

    added query 1 and 2

---
+++ submission/query_2.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE TABLE videet.user_devices_cumulated (
+    user_id BIGINT,
+    browser_type VARCHAR,
+    dates_active ARRAY(DATE),
+    `date` DATE
+)
+WITH (
+    FORMAT = 'PARQUET',
+    PARTITIONING = ARRAY['date']
+)
