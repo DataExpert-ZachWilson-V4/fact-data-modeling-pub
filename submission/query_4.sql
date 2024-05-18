8afbbf6641fc7c6e14ade3c74aa8564146110242
jrsarrat <151387051+jrsarrat@users.noreply.github.com>
2024-05-17 22:28:12 -0400

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -5,18 +5,22 @@ WITH
     FROM
       jrsarrat.user_devices_cumulated
     WHERE
-      DATE = DATE('2023-01-07')
+      date = DATE('2023-01-08')
   )
 SELECT
   user_id,
-  SUM(
-    CASE
-      WHEN CONTAINS(dates_active, sequence_date) THEN POW(2, 32 - DATE_DIFF('day', sequence_date, DATE))
-      ELSE 0
-    END
+  browser_id
+  CAST(
+    SUM(
+      CASE
+        WHEN CONTAINS(dates_active, sequence_date) THEN POW(2, 32 - DATE_DIFF('day', sequence_date, DATE))
+        ELSE 0
+      END
+    ) AS BIGINT
   )
 FROM
   today
-  CROSS JOIN UNNEST (SEQUENCE(DATE('2023-01-01'), DATE('2023-01-07'))) AS t (sequence_date)
+  CROSS JOIN UNNEST (SEQUENCE(DATE('2023-01-01'), DATE('2023-01-08'))) AS t (sequence_date)
 GROUP BY
-  user_id
+  user_id,
+  browser_id
