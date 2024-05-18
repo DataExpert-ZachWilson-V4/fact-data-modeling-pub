d52d64f2f28254d7bbb30f925796d6ac90c6a376
Carlos Laguna <carlos.alr88@gmail.com>
2024-05-17 20:54:20 -0700

    fix browser type

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -2,18 +2,24 @@ WITH
 today AS (
     SELECT * FROM user_devices_cumulated
     WHERE date = DATE('2023-01-05')
+),
+date_list_binary AS 
+(
+    SELECT
+        user_id,
+        browser_type,
+        CAST( SUM(
+            CASE WHEN 
+                CONTAINS(dates_active, sequence_date)
+                THEN POW(2, 32 - DATE_DIFF('day', sequence_date , date))
+                ELSE 0
+            END
+        ) AS BIGINT) AS dates_active
+    FROM today
+    CROSS JOIN UNNEST(SEQUENCE(DATE('2023-01-01'), DATE('2023-01-05'))) AS t(sequence_date)
+    GROUP BY user_id, browser_type
 )
-
-SELECT
-    user_id,
-    CAST( SUM(
-        CASE WHEN 
-            CONTAINS(dates_active, sequence_date)
-            THEN POW(2, 32 - DATE_DIFF('day', sequence_date , date))
-            ELSE 0
-        END
-    ) AS BIGINT)
-    
-FROM today
-CROSS JOIN UNNEST(SEQUENCE(DATE('2023-01-01'), DATE('2023-01-05'))) AS t(sequence_date)
-GROUP BY user_id
+SELECT 
+    *,
+     TO_BASE(dates_active, 2) AS dates_active_binary
+FROM date_list_binary
