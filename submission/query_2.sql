0d7575e5848b73550294528652b4e815b8bedfbc
shettyabhi <62216975+shettyabhi@users.noreply.github.com>
2024-05-19 12:23:29 -0500

    Update query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,4 +1,4 @@
-CREATE TABLE abhishekshetty.user_devices_cumulated (
+CREATE or REPLACE TABLE abhishekshetty.user_devices_cumulated (
   user_id BIGINT,
   browser_type VARCHAR,
   dates_active ARRAY(DATE),
