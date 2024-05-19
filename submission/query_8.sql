3180f4fc4d754edc24a984976da09a31e53a081d
raj-gupta09 <rajk.gupta0910@gmail.com>
2024-05-19 00:17:04 -0400

    Update query_8.sql

--- submission/query_8.sql
+++ submission/query_8.sql
@@ -1,5 +1,8 @@
+-- Insert into the host_activity_reduced table
 INSERT INTO raj.host_activity_reduced
+-- Common Table Expressions (CTEs) to handle data from previous day and current day
 WITH
+    -- CTE for data from the previous day
     yesterday AS (
         SELECT
             host,
@@ -9,8 +12,9 @@ WITH
         FROM
             raj.host_activity_reduced
         WHERE
-            month_start = '2023-01-01'
+            month_start = '2023-01-01' -- Select data for the previous month
     ),
+    -- CTE for data from the current day
     today AS (
         SELECT
             host,
@@ -20,21 +24,22 @@ WITH
         FROM
             raj.daily_web_metrics
         WHERE
-            date = DATE '2023-01-02'
+            date = DATE '2023-01-02' -- Select data for the current month
     )
+-- Main query to merge data from previous and current days
 SELECT
-    COALESCE(y.host, t.host) AS host,
-    COALESCE(y.metric_name, t.metric_name) AS metric_name,
+    COALESCE(y.host, t.host) AS host, -- Choose the non-null host value between yesterday and today
+    COALESCE(y.metric_name, t.metric_name) AS metric_name, -- Choose the non-null metric_name value between yesterday and today
     COALESCE(
-        y.metric_array,
+        y.metric_array, -- Use the metric_array value from yesterday if not null
         REPEAT(
             NULL,
             CAST(
-                DATE_DIFF('day', DATE '2023-01-01', t.date) AS INTEGER
+                DATE_DIFF('day', DATE '2023-01-01', t.date) AS INTEGER -- Calculate the number of days between today's date and the start of the month
             )
         )
-    ) || ARRAY[t.metric_value] AS metric_array,
-    '2023-01-01' AS month_start
+    ) || ARRAY[t.metric_value] AS metric_array, -- Concatenate metric_array from yesterday with the current day's metric_value
+    '2023-01-01' AS month_start -- Set the month_start value to the start of the month
 FROM
     yesterday y
-    FULL OUTER JOIN today t ON y.host = t.host AND y.metric_name = t.metric_name
+    FULL OUTER JOIN today t ON y.host = t.host AND y.metric_name = t.metric_name -- Perform a full outer join between yesterday and today on host and metric_name
