c0f6cd3ab4ebe39f3b419ed569d9ec88468dc2a0
xeno <52781411+brianmori@users.noreply.github.com>
2024-05-19 16:05:54 +0200

    q7 fix

--- submission/query_8.sql
+++ submission/query_8.sql
@@ -21,10 +21,12 @@ WITH
         WHERE
             month_start = '2023-01-02'
     )
+-- Combine the data from the previous and current day
 SELECT
-    t.host,
-    COALESCE(y.metric_name, t.metric_name) AS metric_name
+    COALESCE(y.host, t.host) AS host,
+    COALESCE(y.metric_name, t.metric_name) AS metric_name,
+    COALESCE(y.metric_array, ARRAY[NULL]) || ARRAY[t.metric_value] AS metric_array,
+    '2023-01-01' AS month_start
 FROM
     yesterday y
-    FULL OUTER JOIN today t ON y.host = t.host
-    AND y.metric_name = t.metric_name
+    FULL OUTER JOIN today t ON y.host = t.host AND y.metric_name = t.metric_name
