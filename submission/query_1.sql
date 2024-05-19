ac765a7d2cff08b5a0087e971f792bed7f5ca6d7
meetapandit <meeta.pandit890@gmail.com>
2024-05-19 15:47:11 -0700

    initial commit of hw2

---
+++ submission/query_1.sql
@@ -0,0 +1,40 @@
+WITH ranking_cte AS (
+SELECT *
+    -- compute row number for each record grouping by game_id, team_id and player_id
+    -- records with same key will have row numbers serially
+     , ROW_NUMBER() OVER(PARTITION BY game_id, team_id, player_id) row_cnt
+FROM bootcamp.nba_game_details
+)
+-- select all columns from the ranking cte and filter for row_cnt == 1
+-- This condition will filter out all duplicate records for the same key
+SELECT game_id
+     , team_id
+     , team_abbreviation
+     , team_city
+     , player_id
+     , player_name
+     , nickname
+     , start_position
+     , comment
+     , min
+     , fgm
+     , fga
+     , fg_pct
+     , fg3m
+     , fg3a
+     , fg3_pct
+     , ftm
+     , fta
+     , ft_pct
+     , oreb
+     , dreb
+     , reb
+     , ast
+     , stl
+     , blk
+     , "to"
+     , pf
+     , pts
+     , plus_minus
+FROM ranking_cte
+WHERE row_cnt = 1
---
+++ submission/query_7.sql
@@ -0,0 +1,16 @@
+CREATE TABLE meetapandit89096646.host_activity_reduced (
+-- host activity reduced to store monthly hosts active in a single row
+host varchar,
+-- metric name based on pages visited
+metric_name varchar,
+-- number of hosts visisted by day for a given month
+metric_array array(integer),
+-- month start day snapshot for given month
+month_start varchar
+)
+WITH (
+    -- format to store the file in Apache Iceberg
+  format = 'PARQUET',
+  -- records with same metric_name and month start day will be stored in same file partition
+  partitioning = ARRAY['metric_name', 'month_start']
+)
