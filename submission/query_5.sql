4c0910ff17c0fee2d9567c398deda618a8247f87
shashankkongara <121494824+shashankkongara@users.noreply.github.com>
2024-05-19 15:26:39 -0400

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,4 +1,4 @@
--- Creates a cumulated daily fact table grouped by host.
+-- Creates a cumulated daily fact table grouped by host
 -- This table is designed to track host activity, partitioned by date for optimal query performance
 CREATE OR REPLACE TABLE shashankkongara.hosts_cumulated (
     host VARCHAR,                        -- Column to store host name
