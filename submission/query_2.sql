684a1c4bed34c207d9e5bd102b52bdfa26345ebd
shettyabhi <62216975+shettyabhi@users.noreply.github.com>
2024-05-19 12:26:02 -0500

    Update query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -2,7 +2,7 @@ CREATE or REPLACE TABLE abhishekshetty.user_devices_cumulated (
   user_id BIGINT,
   browser_type VARCHAR,
   dates_active ARRAY(DATE),
-  date DATE
+  date DATE 
 )
 WITH (
   FORMAT = 'PARQUET',
