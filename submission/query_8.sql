f701614217dbea7199c1825559195e3d4d6e5aa2
Nancy Castillo <nancycastillo@Nancys-MacBook-Pro.local>
2024-05-19 16:08:20 -0400

    typo

--- submission/query_8.sql
+++ submission/query_8.sql
@@ -34,7 +34,7 @@ SELECT
   '2023-08-01' AS month_start
 FROM
   today t
-  FULL OUTER JOIN yesterday y ON t.user_id = y.user_idg
+  FULL OUTER JOIN yesterday y ON t.host = y.host
   AND t.metric_name = y.metric_name
 WHERE
   CARDINALITY(
