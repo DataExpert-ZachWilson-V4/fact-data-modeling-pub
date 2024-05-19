a34e857e071d0f46f85efbf7c8878fca7f94f090
raj-gupta09 <rajk.gupta0910@gmail.com>
2024-05-18 23:12:27 -0400

    Update query_7.sql

---
+++ submission/query_7.sql
@@ -0,0 +1,24 @@
+
+-- Create or replace the table host_activity_reduced
+CREATE OR REPLACE TABLE raj.host_activity_reduced (
+    
+    -- The host identifier
+    host VARCHAR,
+    
+    -- The name of the metric
+    metric_name VARCHAR,
+    
+    -- Array of integers representing the metric values
+    metric_array ARRAY(INTEGER),
+    
+    -- The start of the month in 'YYYY-MM' format
+    month_start VARCHAR
+)
+WITH (
+    -- Specify the file format for the table
+    FORMAT = 'PARQUET',
+    
+    -- Partition the table by the month_start column for performance optimization
+    partitioning = ARRAY['metric_name','month_start']
+)
+
