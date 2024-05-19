8be9470a230f84d80b1e2b4f08e371d9d43d8c98
DeeptiVarghese <31417684+DeeptiVarghese@users.noreply.github.com>
2024-05-18 19:27:01 -0700

    Update query_1.sql

---
+++ submission/query_1.sql
@@ -0,0 +1,9 @@
+with repeatedrows as (
+    select
+        *,
+        row_number() over (partition by game_id,team_id,player_id ) as rownum
+    from bootcamp.nba_game_details
+)
+select *
+from repeatedrows
+where rownum=1
