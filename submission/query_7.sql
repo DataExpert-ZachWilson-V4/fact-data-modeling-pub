461674922627c4ed1341ad3f13b5cbcc8885f7d5
Alissa Del Toro <34179808+alissadeltoro@users.noreply.github.com>
2024-05-19 11:28:16 -0400

    Insert query_7.sql

---
+++ submission/query_7.sql
@@ -0,0 +1,13 @@
+-- Reduced Host Fact Array DDL (query_7.sql)
+-- This DDL statement creates a monthly host_activity_reduced table to store reduced host activity data.
+
+-- Create the host_activity_reduced table
+CREATE TABLE alissabdeltoro.host_activity_reduced (
+    host VARCHAR,
+    metric_name VARCHAR,
+    metric_array ARRAY(INTEGER),
+    month_start VARCHAR
+) WITH (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['metric_name', 'month_start']
+)
