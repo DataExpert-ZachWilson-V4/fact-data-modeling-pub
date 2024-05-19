b015fd608ce0b5244ac0f31678165ce04d8b6c33
DeeptiVarghese <31417684+DeeptiVarghese@users.noreply.github.com>
2024-05-18 19:27:30 -0700

    Update query_2.sql

---
+++ submission/query_2.sql
@@ -0,0 +1,8 @@
+create or replace table deeptianievarghese22866.user_devices_cumulated (
+  user_id bigint,
+  browser_type varchar,
+  dates_active array(date),
+  date date
+)
+with
+  (format = 'PARQUET', partitioning = array['date'])
