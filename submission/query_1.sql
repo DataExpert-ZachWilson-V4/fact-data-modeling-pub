80d75f599f74c19827de22bee966a6fffb4e97bf
raj-gupta09 <rajk.gupta0910@gmail.com>
2024-05-18 20:18:00 -0400

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -5,7 +5,7 @@ WITH unique_games AS (
         -- Assign a unique row number to each row within the partition
         ROW_NUMBER() OVER (PARTITION BY game_id, team_id, player_id ORDER BY game_id) AS row_num
     FROM
-        nba_game_details
+        bootcamp.nba_game_details
 )
 
 -- Select only the first occurrence of each combination of game_id, team_id, and player_id
