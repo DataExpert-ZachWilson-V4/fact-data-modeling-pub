057e530ca2988ae494e33f49903b167578cd6d36
bgar8 <92387983+bgar8@users.noreply.github.com>
2024-05-20 16:53:11 -0700

    Update query_8.sql
    fixing the date in the "today" CTE

--- submission/query_8.sql
+++ submission/query_8.sql
@@ -7,12 +7,12 @@ WHERE month_start = '2023-08-01'
 today AS (
   SELECT *
   FROM bgar.daily_web_metrics
-  WHERE date = '2023-08-01'
+  WHERE date = DATE('2023-08-01')
 )
 SELECT
   COALESCE(t.host, y.host) as host,
   COALESCE(t.metric_name, y.metric_name) as metric_name,
-  COALESCE(y.metric_array, REPEAT(NULL, CAST(DATE_DIFF('day', '2023-08-01', t.date) AS INTEGER))) || ARRAY[t.metric_value] as metric_array,
+  COALESCE(y.metric_array, REPEAT(NULL, CAST(DATE_DIFF('day', DATE('2023-08-01'), t.date) AS INTEGER))) || ARRAY[t.metric_value] as metric_array,
   '2023-08-01' as month_start
 FROM today t 
 FULL OUTER JOIN yesterday y 
