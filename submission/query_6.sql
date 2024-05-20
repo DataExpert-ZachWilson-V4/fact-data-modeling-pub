ff475d80e6c81cda1b9f135c8de203738e27d8f1
derekleung0399 <147735897+derekleung0399@users.noreply.github.com>
2024-05-20 16:00:35 +0800

    Update query_6.sql

--- submission/query_6.sql
+++ submission/query_6.sql
@@ -1,5 +1,10 @@
 INSERT INTO
   derekleung.hosts_cumulated
+-- CTE layers:
+-- Situation: hosts_cumulated has been filled up to 2023-01-02, now filling 2023-01-03 incrementally
+-- yesterday: hosts_cumulated table with 2nd most recent info
+-- today: summarized data of today with same format as hosts_cumulated for easy adaptation
+-- SELECT: join yesterday and today and coalesce/concatenate appropriate entries to show updated results
 WITH
   yesterday AS (
     SELECT
@@ -7,7 +12,7 @@ WITH
     FROM
       derekleung.hosts_cumulated
     WHERE
-      DATE = DATE('2023-01-06')
+      DATE = DATE('2023-01-02')
   ),
   today AS (
     SELECT
@@ -17,7 +22,7 @@ WITH
     FROM
       derekleung.web_events
     WHERE
-      date_trunc('day', event_time) = DATE('2023-01-07')
+      date_trunc('day', event_time) = DATE('2023-01-03')
     GROUP BY
       host,
       CAST(date_trunc('day', event_time) AS DATE)
@@ -28,7 +33,7 @@ SELECT
     WHEN y.host_activity_datelist IS NOT NULL THEN ARRAY[t.event_date] || y.host_activity_datelist
     ELSE ARRAY[t.event_date]
   END AS dates_active,
-  DATE('2023-01-07') AS DATE
+  DATE('2023-01-03') AS DATE
 FROM
   yesterday y
   FULL OUTER JOIN today t ON y.host = t.host
