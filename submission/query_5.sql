53e94810b603c378c00dc25b26df37e6d60517cb
hdamerla <harshithadamerla@gmail.com>
2024-05-20 16:28:43 -0500

    Update query_5.sql

---
+++ submission/query_5.sql
@@ -0,0 +1,10 @@
+--HW2 query_5
+CREATE TABLE hdamerla.hosts_cumulated(
+  host VARCHAR,
+  host_activity_datelist ARRAY(DATE),
+  date DATE
+)
+WITH (
+  format = 'PARQUET',
+  partitioning = ARRAY['date']
+)
