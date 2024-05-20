9dc4bf648e75b4bd2ad189443face73c0aa5a6ae
Dennis Gera <geradennis@gmail.com>
2024-05-20 12:19:05 -0300

    feat: use suggestions

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,18 +1,18 @@
-WITH 
-
-game_details_numbered AS (
+-- Using a Common Table Expression (CTE) to create a temporary table with row numbers
+WITH game_details_numbered AS (
     SELECT
         *,
+        -- Assign a unique row number to each row within a partition of game_id, team_id, and player_id
         ROW_NUMBER() over (
             PARTITION BY game_id,
             team_id,
             player_id
-        ) AS rn
+        ) AS row_number
     FROM
         bootcamp.nba_game_details
 )
 
+-- Select all rows where the row number is 1, effectively removing duplicates
 SELECT *
 FROM game_details_numbered
-WHERE TRUE 
-    AND rn = 1
+WHERE row_number = 1
