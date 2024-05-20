580f04b0203d8445fce7f10a60271436df388397
shettyabhi <62216975+shettyabhi@users.noreply.github.com>
2024-05-20 16:19:27 -0500

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,9 +1,16 @@
-create or replace table abhishekshetty.hosts_cumulated
+-- Create or replace the hosts_cumulated table
+create or replace table hosts_cumulated
 (
+  -- Define the host column
   host varchar,
+  
+  -- Define the host_activity_datelist column
   host_activity_datelist array(date),
+  
+  -- Define the date column
   date date
 )
+-- Specify the table format and partitioning
 WITH (
 format = 'PARQUET',
 partitioning = ARRAY['date']
