abd5c19d40b15ff63413674b5f484a9df8b6aa2d
phabrahao <44507879+phabrahao@users.noreply.github.com>
2024-05-20 17:07:54 -0300

    Add files via upload

---
+++ submission/query_8.sql
@@ -0,0 +1,36 @@
+WITH
+    yesterday AS (
+        SELECT
+            *
+        FROM
+            phabrahao.host_activity_reduced
+        WHERE
+            month_start = '2023-08-01'
+    ),
+    today AS (
+        SELECT
+            *
+        FROM
+            phabrahao.daily_web_metrics
+        WHERE
+            DATE = date('2023-08-01')
+    )
+SELECT
+    COALESCE(t.host, y.host) AS host,
+    COALESCE(t.metric_name, y.metric_name) AS metric_name,
+    COALESCE(
+        y.metric_array,
+        REPEAT(
+            NULL,
+            CAST(
+                DATE_DIFF('day', DATE('2023-08-01'), t.date) AS INTEGER
+            )
+        )
+    ) || ARRAY[t.metric_value] AS metric_array,
+    '2023-08-01' AS month_start
+FROM
+    today t
+    FULL OUTER JOIN yesterday y ON t.host = y.host
+    AND t.metric_name = y.metric_name
+
+    
