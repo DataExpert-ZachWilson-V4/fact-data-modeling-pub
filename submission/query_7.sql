0d860d3beb254a3a95ad90c8ba351b31e5fb08c7
derekleung0399 <147735897+derekleung0399@users.noreply.github.com>
2024-05-20 16:11:43 +0800

    Update query_7.sql

---
+++ submission/query_7.sql
@@ -0,0 +1,12 @@
+--quick intro: a reduced fact data table with 1 row per month per host per metric
+CREATE OR REPLACE TABLE derekleung.monthly_array_web_metrics (
+  host VARCHAR,
+  metric_name VARCHAR,
+  metric_array ARRAY(INTEGER),
+  month_start VARCHAR
+)
+WITH
+  (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['metric_name', 'month_start']
+  )
