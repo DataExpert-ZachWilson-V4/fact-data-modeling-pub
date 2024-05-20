bdaae54f972bf9d9fab5df08e12af832e51262a1
Nikhil Sahni <54640886+nickable90@users.noreply.github.com>
2024-05-20 18:22:42 -0400

    Update query_2.sql

---
+++ submission/query_2.sql
@@ -0,0 +1,10 @@
+CREATE TABLE nikhilsahni.user_devices_cumulated (
+user_id BIGINT,
+browser_type VARCHAR,
+dates_active ARRAY(DATE),
+date DATE
+)
+WITH (
+FORMAT = 'PARQUET',
+PARTITIONING = ARRAY['date']
+)
