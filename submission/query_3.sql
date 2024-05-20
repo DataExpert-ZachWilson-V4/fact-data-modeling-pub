ac161597491339c10fc50cedfe710bf1a9297a1a
Nikhil Sahni <54640886+nickable90@users.noreply.github.com>
2024-05-20 19:56:33 -0400

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,7 +1,7 @@
 INSERT INTO
   nikhilsahni.user_devices_cumulated
 WITH
-  web_events_devices_joined AS (
+  combined AS (
     SELECT
       we.user_id AS user_id,
       we.device_id AS device_id,
@@ -10,6 +10,27 @@ WITH
     FROM
       bootcamp.web_events AS we
       LEFT JOIN bootcamp.devices AS d ON we.device_id = d.device_id
+  ),
+  dupes AS (
+    SELECT
+      user_id,
+      browser_type,
+      event_date,
+      COUNT(*) AS cnt
+    FROM
+      combined
+    GROUP BY
+      user_id,
+      browser_type,
+      event_date
+  ),
+  deduped AS (
+    SELECT
+      user_id,
+      browser_type,
+      event_date
+    FROM
+      dupes
   ),
   yesterday AS (
     SELECT
@@ -21,18 +42,11 @@ WITH
   ),
   today AS (
     SELECT
-      user_id,
-      browser_type,
-      event_date,
-      COUNT(1)
+      *
     FROM
-      web_events_devices_joined
+      deduped
     WHERE
       event_date = DATE('2023-01-01')
-    GROUP BY
-      1,
-      2,
-      3
   )
 SELECT
   COALESCE(y.user_id, t.user_id) AS user_id,
