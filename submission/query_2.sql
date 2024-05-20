76b8422560c24dab2e5964d8668bb78398a4cac3
derekleung0399 <147735897+derekleung0399@users.noreply.github.com>
2024-05-20 14:59:53 +0800

    Update query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,6 +1,6 @@
 --very straightforward
 --browser_type would be filled in by joining web_events table with devices table
-CREATE TABLE derekleung.web_users_cumulated (
+CREATE OR REPLACE TABLE derekleung.web_users_cumulated (
   user_id BIGINT,
   browser_type VARCHAR,
   dates_active ARRAY(DATE),
