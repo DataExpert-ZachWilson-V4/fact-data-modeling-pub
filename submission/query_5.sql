ce3bd05b431e42348f44371c01a35f9807b2e974
derekleung0399 <147735897+derekleung0399@users.noreply.github.com>
2024-05-20 15:44:54 +0800

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,5 +1,5 @@
 --Again very straightforward, host is the major identifier here
-CREATE OR REPLACE TABLE zachwilson.hosts_cumulated (
+CREATE OR REPLACE TABLE derekleung.hosts_cumulated (
   host VARCHAR,
   host_activity_datelist ARRAY(DATE),
   DATE DATE
