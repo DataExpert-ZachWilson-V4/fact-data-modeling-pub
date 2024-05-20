e2c12923172378ae2df355dd5b94b819827a0793
rgindallas <richardgarth68@gmail.com>
2024-05-20 00:42:07 -0500

    Update query_8.sql

--- submission/query_8.sql
+++ submission/query_8.sql
@@ -19,7 +19,7 @@ with
             metric_value,
             date
         from
-            rgindallas.daily_metrics
+            daily_web_metrics
         where
             date = date('2021-01-02') -- iterate through days in month in order
     )
