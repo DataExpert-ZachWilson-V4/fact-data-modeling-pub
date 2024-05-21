b7270457dbee032047ff5635e2dd636221cf3c45
mmarquez225 <63692729+mmarquez225@users.noreply.github.com>
2024-05-20 20:44:22 -0700

    Update query_6.sql

--- submission/query_6.sql
+++ submission/query_6.sql
@@ -3,7 +3,7 @@ INSERT INTO hosts_cumulated
 SELECT
     we.host,
     -- Aggregate event dates into an array, ordered by date in descending order
-    ARRAY_AGG(we.event_date ORDER BY we.event_date DESC) as host_activity_datelist,
+    ARRAY_AGG(DATE(we.event_time) ORDER BY DATE(we.event_time) DESC) as host_activity_datelist,
     CURRENT_DATE as date
 FROM bootcamp.web_events we
 -- Group by host to get unique hosts
