e72b0b0de26ccb0b26bc7422376e2d397ff9da3c
Ibrahim Sherif <hemamehsherif@gmail.com>
2024-05-21 04:22:30 +0300

    added comment

--- submission/query_8.sql
+++ submission/query_8.sql
@@ -2,7 +2,7 @@ INSERT INTO host_activity_reduced
 WITH yesterday AS (
   SELECT *
   FROM host_activity_reduced
-  WHERE month_start = '2023-08-01' -- start of month needed
+  WHERE month_start = '2023-08-01' -- START OF MONTH NEEDED. DOES NOT CHANGE
 ),
 today AS (
   SELECT *
@@ -16,7 +16,7 @@ SELECT
     y.metric_array,
     REPEAT(NULL, CAST(DATE_DIFF('day', DATE('2023-08-01'), t.date) AS INTEGER))
   ) || ARRAY[t.metric_value] AS metric_array,
-  '2023-08-01' AS month_start
+  '2023-08-01' AS month_start -- START OF MONTH NEEDED. DOES NOT CHANGE
 FROM today t
 FULL OUTER JOIN yesterday y 
   ON t.host = y.host AND t.metric_name = y.metric_name
