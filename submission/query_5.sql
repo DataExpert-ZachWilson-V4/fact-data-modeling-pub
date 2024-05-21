0ce52274c2b45d541bfc416ad4cd8314d15e43b0
Nikhil Sahni <54640886+nickable90@users.noreply.github.com>
2024-05-21 14:07:25 -0400

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,9 +1,9 @@
-CREATE TABLE nikhilsahni.hosts_cumulated (
-host VARCHAR,
-host_activity_datelist ARRAY(DATE),
-date DATE
+CREATE OR REPLACE TABLE nikhilsahni.hosts_cumulated (
+    host VARCHAR,                    -- Column to store the host as a VARCHAR
+    host_activity_datelist ARRAY(DATE), -- Column to store an array of dates when the host was active
+    date DATE                        -- Column to store the date of the record
 )
 WITH (
-FORMAT = 'PARQUET',
-PARTITIONING = ARRAY['date']
+    FORMAT = 'PARQUET',              -- Specify the table storage format as Parquet
+    PARTITIONING = ARRAY['date']     -- Partition the table by the date column for performance optimization
 )
