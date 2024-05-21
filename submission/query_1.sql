d8afaf3db68d5b584140f8cba8249b3a506fd177
mmarquez225 <michaelcmarquez@gmail.com>
2024-05-20 22:35:43 -0700

    boot camp

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE TABLE mmarquez225.fct_nba_game_details_deduped AS 
+CREATE OR REPLACE TABLE bootcamp.fct_nba_game_details_deduped AS 
 WITH rn_marking AS (
   SELECT
     *,
     ROW_NUMBER() OVER (PARTITION BY game_id, team_id, player_id) AS rn
     -- This is becuase we need to remove the duplicates based on the columns mentioned
     -- Hence we are using the row_number window function
-  FROM mmarquez225.fct_nba_game_details -- Lab 1 - Fact Table
+  FROM bootcamp.fct_nba_game_details -- Lab 1 - Fact Table
 )
 SELECT *
 FROM rn_marking
