704be76dff6ee22cb7b7b48916052ba0bbb7e00c
hdamerla <harshithadamerla@gmail.com>
2024-05-21 00:06:41 -0500

    Update query_8.sql

--- submission/query_8.sql
+++ submission/query_8.sql
@@ -3,20 +3,28 @@ INSERT into hdamerla.host_activity_reduced
 With yesterday AS (
 select * from 
 hdamerla.host_activity_reduced
-where DATE(month_start) = DATE('2023-08-02')
+where(month_start) =('2023-08-01')
 ), 
 today as (
 select
   *
 from hdamerla.daily_web_metrics
-where DATE(date) = DATE('2023-08-02')
+where (date) = DATE('2023-08-02')
 )
 
 SELECT
 COALESCE(y.host, t.host) as host,
 COALESCE(y.metric_name, t.metric_name) as metric_name,
- COALESCE (y.metric_array, REPEAT(NULL, CAST(DATE_DIFF('day', DATE('2023-08-01'), t.date) AS INTEGER ))) || ARRAY[t.metric_value] as metric_array,
-CAST('2023-08-01' as DATE) as month_start
-from yesterday y
-full outer join today t
-on y.host = t.host and y.metric_name=t.metric_name 
+ COALESCE(
+    y.metric_array,
+    REPEAT(
+      NULL,
+      CAST(
+        DATE_DIFF('day', DATE('2023-08-01'), t.date) AS INTEGER
+      )
+    )
+  ) || ARRAY[t.metric_value] AS metric_array,
+ '2023-08-01' as month_start
+from today t
+full outer join yesterday y
+on y.host = cast(t.host as varchar) and y.metric_name=t.metric_name
