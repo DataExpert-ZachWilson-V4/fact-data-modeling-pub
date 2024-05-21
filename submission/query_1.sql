05053a6dac1dc268f63c500973f43cf2130cb438
hdamerla <harshithadamerla@gmail.com>
2024-05-21 18:54:33 -0500

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -4,7 +4,7 @@
 with deduped_nba_game_details AS (
 select 
 *,
-row_number() OVER (PARTITION BY player_id, game_id, team_id ) AS rownum
+row_number() OVER (PARTITION BY game_id, team_id, player_id order by game_id, team_id, player_id ) AS rownum --de-duping based on the combination of game_id, team_id and player_id
 from bootcamp.nba_game_details
 )
 
