198328a118325525017f7bbb718fe5b08d2f7bb2
Alissa Del Toro <34179808+alissadeltoro@users.noreply.github.com>
2024-05-21 00:41:18 -0400

    Removing schema from query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,8 +1,8 @@
-INSERT INTO alissabdeltoro.user_devices_cumulated
+INSERT INTO user_devices_cumulated
 -- Step 1: Define the yesterday CTE to capture the state of user_devices_cumulated table for the previous day.
 WITH yesterday AS (
     SELECT * 
-    FROM alissabdeltoro.user_devices_cumulated
+    FROM user_devices_cumulated
     WHERE date = DATE '2022-12-31'
 ),
 
@@ -13,8 +13,8 @@ today AS (
         d.browser_type,
         CAST(date_trunc('day', we.event_time) AS DATE) AS event_date,
         COUNT(1) AS event_count
-    FROM academy.bootcamp.web_events we
-    JOIN academy.bootcamp.devices d
+    FROM bootcamp.web_events we
+    JOIN bootcamp.devices d
         ON we.device_id = d.device_id
     WHERE date_trunc('day', we.event_time) = DATE '2023-01-01'
     GROUP BY we.user_id, d.browser_type, CAST(date_trunc('day', we.event_time) AS DATE)
@@ -34,6 +34,3 @@ merged_data AS (
     FULL OUTER JOIN today t 
     ON y.user_id = t.user_id AND y.browser_type = t.browser_type
 )
-
--- Step 4: Insert the merged data into the user_devices_cumulated table
-SELECT * FROM merged_data
