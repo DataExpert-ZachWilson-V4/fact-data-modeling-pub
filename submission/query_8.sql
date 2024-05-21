c0fe408d238da8d97b65e8c5f84543643c04b9a5
mmarquez225 <63692729+mmarquez225@users.noreply.github.com>
2024-05-20 21:17:34 -0700

    Update query_8.sql

--- submission/query_8.sql
+++ submission/query_8.sql
@@ -1,20 +1,36 @@
-WITH monthly_metrics AS (
+--incrementally populate the host_activity_reduced
+INSERT INTO bootcamp.host_activity_reduced
+WITH
+  yesterday AS ( -- get yesterday's data from host_activity_reduced table
     SELECT
-        host,
-        metric_name,
-        -- Aggregate metric values into an array, ordered by metric_date
-        ARRAY_AGG(metric_value ORDER BY metric_date) as metric_array,
-        -- Get the start of the month for each metric_date
-        DATE_TRUNC('month', metric_date) as month_start
-    FROM bootcamp.daily_web_metrics
-    -- Group by host, metric_name, and the start of the month
-    GROUP BY host, metric_name, DATE_TRUNC('month', metric_date)
-)
-INSERT INTO host_activity_reduced (host, metric_name, metric_array, month_start)
+      *
+    FROM
+      bootcamp.host_activity_reduced
+    WHERE
+      month_start = '2023-05-01'
+  ),
+  today AS ( -- get today's data from daily_web_metrics table
+    SELECT
+      *
+    FROM
+      bootcamp.daily_web_metrics 
+    WHERE
+      DATE = DATE('2023-05-02')
+  )
 SELECT
-    host,
-    metric_name,
-    metric_array,
-    -- Convert the month_start date to a string
-    CAST(month_start AS varchar) as month_start
-FROM bootcamp.monthly_metrics
+  COALESCE(t.host, y.host) AS host, --using coalesce function to avoid null values
+  COALESCE(t.metric_name, y.metric_name) AS metric_name, -- name of the metric
+  COALESCE(
+    y.metric_array, 
+    REPEAT(
+      NULL,
+      CAST(
+        DATE_DIFF('day', DATE('2023-05-01'), t.date) AS INTEGER
+      )
+    )
+  ) || ARRAY[t.metric_value] AS metric_array, --if previous data already exists, we append the new data to the existing, if not create a new array
+  '2023-05-01' AS month_start
+FROM
+  today t
+  FULL OUTER JOIN yesterday y ON t.host = y.host --join yesterday's data with today's data on host and metric_name
+  AND t.metric_name = y.metric_name
