ce372c598609a2529876386835ae282fb6b4cd0e
Nikhil Sahni <54640886+nickable90@users.noreply.github.com>
2024-05-21 02:22:47 -0400

    Update query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,10 +1,10 @@
-CREATE TABLE nikhilsahni.user_devices_cumulated (
-user_id BIGINT,
-browser_type VARCHAR,
-dates_active ARRAY(DATE),
-date DATE
+CREATE OR REPLACE TABLE nikhilsahni.user_devices_cumulated (
+    user_id BIGINT,             -- Column to store user IDs as BIGINT
+    browser_type VARCHAR,       -- Column to store the type of browser as a VARCHAR
+    dates_active ARRAY(DATE),   -- Column to store an array of dates when the user was active
+    date DATE                   -- Column to store the date of the record
 )
 WITH (
-FORMAT = 'PARQUET',
-PARTITIONING = ARRAY['date']
+    FORMAT = 'PARQUET',         -- Specify the table storage format as Parquet
+    PARTITIONING = ARRAY['date']-- Partition the table by the date column for performance optimization
 )
