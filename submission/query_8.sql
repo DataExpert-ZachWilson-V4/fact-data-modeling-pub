3f9951d1e73d71bfce9b9886ea16497dae68390d
fayiz_tk <fayiz.tk@zoom.us>
2024-05-21 14:51:38 +0530

    update queries

---
+++ submission/query_8.sql
@@ -0,0 +1,36 @@
+insert into
+    fayiztk.host_activity_reduced
+with
+    yesterday as (
+        select
+            *
+        from
+            fayiztk.host_activity_reduced
+        where
+            month_start = '2023-08-01'
+    ),
+    today as (
+        select
+            *
+        from
+            bootcamp.daily_web_metrics
+        where
+            date = date('2023-08-01')
+    )
+select
+    coalesce(t.host, y.host) as user_id,
+    coalesce(t.metric_name, y.metric_name) as metric_name,
+    coalesce(
+        y.metric_array,
+        repeat(
+            null,
+            cast(
+                date_diff('day', date('2023-08-01'), t.date) as integer
+            )
+        )
+    ) || array[t.metric_value] as metric_array,
+    '2023-08-01' as month_start
+from
+    today t
+    full outer join yesterday y on t.host = y.host
+    and t.metric_name = y.metric_name
