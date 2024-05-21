3c3d39ea1247f39016ef6741dfbe38f5220ec849
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2024-05-21 17:04:28 -0600

    Update query_1.sql
    De-dupe Query (query_1.sql)

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,1 +1,24 @@
-
+-- select all distinct records from the "bootcamp.nba_game_details"
+WITH
+    deduped_table AS (
+        -- CTE to add a row number to each record 
+        -- partitioned by game_id, team_id, and player_id
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY
+                    game_id,
+                    team_id,
+                    player_id
+                ORDER BY
+                    player_name
+            ) AS row_number
+        FROM
+            bootcamp.nba_game_details
+    )
+SELECT
+   *
+FROM
+    deduped_table
+WHERE
+    row_number = 1 
