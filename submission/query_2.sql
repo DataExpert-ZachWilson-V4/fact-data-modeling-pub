0c96fd4d2def51a684d7f6195429fec942aa3937
rgindallas <richardgarth68@gmail.com>
2024-05-21 14:04:31 -0500

    Update query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,10 +1,13 @@
 -- create cumulating user activity table by device
-CREATE
-OR REPLACE TABLE RGINDALLAS.USER_DEVICES_CUMULATED (
-    USER_ID BIGINT,
-    BROWSER_TYPE VARCHAR,
-    DATES_ACTIVE ARRAY(DATE),
-    DATE DATE
-)
-WITH
-    (FORMAT='PARQUET', PARTITIONING=ARRAY['date'])
+-- This DDL statement creates a table to store cumulating user activity by device.
+-- The table is the result of joining the devices table onto the web_events table to get both the user_id and the browser_type.
+
+-- Create the user_devices_cumulated table
+CREATE TABLE user_devices_cumulated (
+    user_id BIGINT,                -- User identifier
+    browser_type VARCHAR,          -- Type of browser used by the user
+    dates_active ARRAY(DATE),      -- Array of dates when the user was active
+    date DATE                      -- The date of the latest activity
+) WITH (
+    format = 'PARQUET',            -- Storage format
+    partitioning = ARRAY['date']   -- Partitioning by date for efficient querying
