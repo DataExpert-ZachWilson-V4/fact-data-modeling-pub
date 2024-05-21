7a151be3fba2c866a749ecb5257ae1a895b621ea
amaliahnation <79658792+amaliahnation@users.noreply.github.com>
2024-05-20 23:43:02 -0500

    Hosts cumulated table creation

---
+++ submission/query_5.sql
@@ -0,0 +1,10 @@
+-- Creates a table of cumilated hosts
+CREATE
+OR REPLACE TABLE hosts_cumulated (
+    host VARCHAR, -- host name variable
+    host_activity_datelist ARRAY(DATE), -- an array of host activity dates
+    date DATE -- Column to store the date of the record
+) WITH (
+    FORMAT = 'PARQUET',-- Stores data in PARQUET format
+    partitioning = ARRAY ['date'] -- Partitions the table by the "date" column
+)
