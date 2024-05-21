960bf4d15a592a612ba160ebc2c8671f4500a9fe
Andres Kammerath <andreskammerath@gmail.com>
2024-05-21 14:12:18 -0300

    Fix table name

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS andreskammerath.monthly_array_web_metrics (
+CREATE TABLE IF NOT EXISTS andreskammerath.host_activity_reduced (
     user_id BIGINT,
     metric_name VARCHAR,
     metric_array ARRAY(INTEGER),
