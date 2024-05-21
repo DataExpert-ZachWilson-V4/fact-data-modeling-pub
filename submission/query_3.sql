683ec6710789c9ff87f992a0ed0128926cd54a00
rgindallas <richardgarth68@gmail.com>
2024-05-21 14:02:15 -0500

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,44 +1,49 @@
--- query to incrementally populate the cumulative user devices activity datelist table
--- min date in web_events table is '2021-01-02'
 INSERT INTO
-    RGINDALLAS.USER_DEVICES_CUMULATED
+    RGINDALLAS.USER_DEVICES_CUMULATED (USER_ID, BROWSER_TYPE, DATES_ACTIVE, DATE)
+    -- Step 1: Define the yesterday CTE to capture the state of user_devices_cumulated table for the previous day.
 WITH
-    PREV_DAY AS (
+    YESTERDAY AS (
         SELECT
             *
         FROM
             RGINDALLAS.USER_DEVICES_CUMULATED
         WHERE
-            DATE=DATE('2021-01-01') -- @start_date
+            DATE=DATE '2022-12-31'
     ),
-    CURR_DAY AS (
+    -- Step 2: Define the today CTE to capture events from the web_events table for the current day.
+    TODAY AS (
         SELECT
-            USER_ID,
-            BROWSER_TYPE,
-            CAST(EVENT_TIME AS DATE) AS EVENT_DATE,
+            WE.USER_ID,
+            D.BROWSER_TYPE,
+            CAST(DATE_TRUNC('day', WE.EVENT_TIME) AS DATE) AS EVENT_DATE,
             COUNT(1) AS EVENT_COUNT
         FROM
-            BOOTCAMP.WEB_EVENTS W
-            JOIN BOOTCAMP.DEVICES D ON W.DEVICE_ID=D.DEVICE_ID
+            BOOTCAMP.WEB_EVENTS WE
+            JOIN BOOTCAMP.DEVICES D ON WE.DEVICE_ID=D.DEVICE_ID
         WHERE
-            CAST(EVENT_TIME AS DATE)=DATE('2021-01-02') --@start_date + 1
+            DATE_TRUNC('day', WE.EVENT_TIME)=DATE '2023-01-01'
         GROUP BY
-            1,
-            2,
-            3
+            WE.USER_ID,
+            D.BROWSER_TYPE,
+            CAST(DATE_TRUNC('day', WE.EVENT_TIME) AS DATE)
+    ),
+    -- Step 3: Insert into the user_devices_cumulated table by merging yesterday's data with today's data.
+    MERGED_DATA AS (
+        SELECT
+            COALESCE(Y.USER_ID, T.USER_ID) AS USER_ID,
+            COALESCE(Y.BROWSER_TYPE, T.BROWSER_TYPE) AS BROWSER_TYPE,
+            CASE
+                WHEN Y.DATES_ACTIVE IS NOT NULL THEN ARRAY[T.EVENT_DATE]||Y.DATES_ACTIVE
+                ELSE ARRAY[T.EVENT_DATE]
+            END AS DATES_ACTIVE,
+            DATE '2023-01-01' AS DATE
+        FROM
+            YESTERDAY Y
+            FULL OUTER JOIN TODAY T ON Y.USER_ID=T.USER_ID
+            AND Y.BROWSER_TYPE=T.BROWSER_TYPE
     )
+    -- Step 4: Insert the merged data into the user_devices_cumulated table
 SELECT
-    COALESCE(C.USER_ID, P.USER_ID) AS USER_ID,
-    COALESCE(C.BROWSER_TYPE, P.BROWSER_TYPE) AS BROWSER_TYPE,
-    CASE
-        WHEN P.DATES_ACTIVE IS NOT NULL
-        AND C.EVENT_DATE IS NOT NULL THEN ARRAY[C.EVENT_DATE]||P.DATES_ACTIVE
-        WHEN P.DATES_ACTIVE IS NOT NULL
-        AND C.EVENT_DATE IS NULL THEN P.DATES_ACTIVE
-        WHEN P.DATES_ACTIVE IS NULL THEN ARRAY[C.EVENT_DATE]
-    END AS DATES_ACTIVE,
-    COALESCE(C.EVENT_DATE, P.DATE+INTERVAL '1' DAY) AS DATE
+    *
 FROM
-    PREV_DAY P
-    FULL OUTER JOIN CURR_DAY C ON P.USER_ID=C.USER_ID
-    AND P.BROWSER_TYPE=C.BROWSER_TYPE
+    MERGED_DATA
