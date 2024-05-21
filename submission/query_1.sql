62bcf6207bd5e675bc25d93dc278660438957c6e
mmarquez225 <michaelcmarquez@gmail.com>
2024-05-20 22:43:12 -0700

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -5,7 +5,7 @@ WITH rn_marking AS (
     ROW_NUMBER() OVER (PARTITION BY game_id, team_id, player_id) AS rn
     -- This is becuase we need to remove the duplicates based on the columns mentioned
     -- Hence we are using the row_number window function
-  FROM bootcamp.fct_nba_game_details -- Lab 1 - Fact Table
+  FROM bootcamp.nba_game_details -- Lab 1 - Fact Table
 )
 SELECT *
 FROM rn_marking
