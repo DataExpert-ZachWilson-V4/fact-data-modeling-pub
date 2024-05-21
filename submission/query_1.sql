69624c630f66d59fe2081ad9c6d2bb55d24b81fd
bgar8 <92387983+bgar8@users.noreply.github.com>
2024-05-20 17:33:46 -0700

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,9 +1,15 @@
-CREATE TABLE bgar.hosts_cumulated (
-    host VARCHAR,
-    host_activity_datelist ARRAY(date),
-    date DATE
+WITH nba_game_details_distinct AS (
+SELECT
+  ROW_NUMBER() OVER (
+    PARTITION BY
+      game_id,
+      team_id,
+      player_id
+  ) AS distinct_row_number,
+  *
+FROM bootcamp.nba_game_details
 )
-WITH ( 
-    format = 'PARQUET',
-    partitioning = ARRAY['date']
-)
+SELECT
+  *
+FROM nba_game_details_distinct
+WHERE distinct_row_number = 1
