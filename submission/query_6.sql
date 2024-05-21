da91e5c5fe63703c35045324949a8b1253aff117
Alissa Del Toro <34179808+alissadeltoro@users.noreply.github.com>
2024-05-21 01:58:30 -0400

    Removing schema from query_6.sql
    + linting and general cleanup

--- submission/query_6.sql
+++ submission/query_6.sql
@@ -1,36 +1,49 @@
 -- Host Activity Datelist Implementation (query_6.sql)
 -- This query incrementally populates the hosts_cumulated table from the web_events table.
 
-INSERT INTO hosts_cumulated
-    
+INSERT INTO hosts_cumulated (host, host_activity_datelist, date)
 -- Step 1: Define the yesterday CTE to capture the state of hosts_cumulated table for the previous day.
-WITH yesterday AS (
-    SELECT * 
-    FROM hosts_cumulated
-    WHERE date = DATE '2022-12-31'
-),
-
--- Step 2: Define the today CTE to capture events from the web_events table for the current day.
-today AS (
-    SELECT 
-        host,
-        CAST(date_trunc('day', event_time) AS DATE) AS event_date,
-        COUNT(1)
-    FROM bootcamp.web_events
-    WHERE date_trunc('day', event_time) = DATE '2023-01-01'
-    GROUP BY host, CAST(date_trunc('day', event_time) AS DATE)
-)
-
--- Step 3: Insert into the hosts_cumulated table by merging yesterday's data with today's data.
-SELECT 
-    COALESCE(y.host, t.host) AS host,
-    -- Update the host_activity_datelist array by adding today's event_date to the front of the list.
-    CASE 
+WITH
+  yesterday AS (
+    SELECT
+      host,
+      host_activity_datelist
+    FROM
+      hosts_cumulated
+    WHERE
+      DATE = DATE '2023-04-08'
+  ),
+  -- Step 2: Define the today CTE to capture events from the web_events table for the current day.
+  today AS (
+    SELECT
+      host,
+      CAST(date_trunc('day', event_time) AS DATE) AS event_date
+    FROM
+      bootcamp.web_events
+    WHERE
+      date_trunc('day', event_time) = DATE '2023-04-09'
+    GROUP BY
+      host,
+      CAST(date_trunc('day', event_time) AS DATE)
+  ),
+  -- Step 3: Merge yesterday's data with today's data.
+  merged_data AS (
+    SELECT
+      COALESCE(y.host, t.host) AS host,
+      CASE
         WHEN y.host_activity_datelist IS NOT NULL THEN ARRAY[t.event_date] || y.host_activity_datelist
         ELSE ARRAY[t.event_date]
-    END AS host_activity_datelist,
-    -- Set the date to today's date.
-    DATE '2023-01-01' AS date
-FROM yesterday y
-FULL OUTER JOIN today t 
-ON y.host = t.host
+      END AS host_activity_datelist,
+      DATE '2023-04-09' AS date
+    FROM
+      yesterday y
+      FULL OUTER JOIN today t ON y.host = t.host
+  )
+
+-- Insert the merged data for the current day
+SELECT
+  host,
+  host_activity_datelist,
+  date
+FROM
+  merged_data
