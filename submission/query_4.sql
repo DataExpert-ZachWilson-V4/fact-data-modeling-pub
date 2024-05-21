e88f4648a13246b5b6bc07331552120648f9e5b8
Alissa Del Toro <34179808+alissadeltoro@users.noreply.github.com>
2024-05-21 01:08:21 -0400

    Update query_4.sql
    Added additional GROUP BY browser_type
    Linted + commented code

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -3,42 +3,59 @@
 
 -- Step 1: Fetch today's data from user_devices_cumulated table
 WITH today AS (
-    SELECT * 
-    FROM alissabdeltoro.user_devices_cumulated
-    WHERE date = DATE '2023-01-02'
+    SELECT 
+        user_id,
+        browser_type,
+        dates_active,
+        date
+    FROM 
+        user_devices_cumulated
+    WHERE 
+        date = DATE '2023-01-07'
 ),
 
--- Step 2: Calculate history integer representation
+-- Step 2: Unnest the dates_active array and calculate the history integer representation
 date_list_int AS (
     SELECT 
         user_id,
-        -- Calculate the history integer representation
-        CAST(
-            SUM(
-                CASE 
-                    WHEN ARRAY_JOIN(dates_active, ',') LIKE '%' || CAST(sequence_date AS VARCHAR) || '%' THEN 
-                        POW(2, 30 - DATE_DIFF('day', sequence_date, DATE '2023-01-03'))
-                    ELSE 0
-                END
-            ) AS BIGINT
+        browser_type,
+        SUM(
+            CASE 
+                WHEN dates_active_element IS NOT NULL THEN CAST(POW(2, 30 - DATE_DIFF('day', dates_active_element, DATE '2023-01-07')) AS BIGINT)
+                ELSE 0
+            END
         ) AS history_int
-    FROM today
-    -- Generate a sequence of dates from '2023-01-01' to '2023-01-02' and unnest them
-    CROSS JOIN UNNEST(SEQUENCE(DATE '2023-01-01', DATE '2023-01-02')) AS t(sequence_date)
-    GROUP BY user_id
+    FROM 
+        today
+        CROSS JOIN UNNEST(dates_active) AS t(dates_active_element)
+    GROUP BY 
+        user_id, 
+        browser_type
 ),
 
 -- Step 3: Select final fields with additional computations
 final_output AS (
     SELECT 
-        *,
+        user_id,
+        browser_type,
+        history_int,
         -- Convert history_int to binary representation
         TO_BASE(history_int, 2) AS history_in_binary,
         -- Count the number of active days using bitwise operation
         BIT_COUNT(history_int, 32) AS num_days_active
-    FROM date_list_int
+    FROM 
+        date_list_int
 )
 
 -- Select the final fields
-SELECT *
-FROM final_output
+SELECT 
+    user_id,
+    browser_type,
+    history_int,
+    history_in_binary,
+    num_days_active
+FROM 
+    final_output
+ORDER BY 
+    user_id, 
+    browser_type
