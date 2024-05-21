840fa0e2ae0c8d0f60c0aa194d2133fae6c36fb8
rgindallas <richardgarth68@gmail.com>
2024-05-21 13:40:33 -0500

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -1,39 +1,39 @@
 -- convert the date list implementation (dates_active) into the base-2 integer representation 
-with
-    curr as (
-        select
-            user_id,
-            browser_type,
-            dates_active,
-            date
-        from
-            rgindallas.user_devices_cumulated
-        where
-            date = date('2021-01-31') -- @end_date of period we'd like to represent in base-2 integer representation; goal is to take dates_active and transform into more compact representation of history; this record has the history as an array of dates called dates_active
+WITH
+    CURR AS (
+        SELECT
+            USER_ID,
+            BROWSER_TYPE,
+            DATES_ACTIVE,
+            DATE
+        FROM
+            RGINDALLAS.USER_DEVICES_CUMULATED
+        WHERE
+            DATE=DATE('2021-01-31') -- @end_date of period we'd like to represent in base-2 integer representation; goal is to take dates_active and transform into more compact representation of history; this record has the history as an array of dates called dates_active
     ),
-    conversion as (
-        select
-            user_id,
-            browser_type,
-            cast(
-                case
-                    when contains(dates_active, sequence_day) then pow(
+    CONVERSION AS (
+        SELECT
+            USER_ID,
+            BROWSER_TYPE,
+            CAST(
+                CASE
+                    WHEN CONTAINS(DATES_ACTIVE, SEQUENCE_DAY) THEN POW(
                         2,
-                        30 - date_diff('day', sequence_day, date) -- 30 will depend on the time period we would like to represent in binary; if it is n days we'd like to represent then fill in n - 1 here; since I'd like to do the month of January which has 31 days I have put 30 here; 
+                        30-DATE_DIFF('day', SEQUENCE_DAY, DATE) -- 30 will depend on the time period we would like to represent in binary; if it is n days we'd like to represent then fill in n - 1 here; since I'd like to do the month of January which has 31 days I have put 30 here; 
                     )
-                    else 0
-                end as bigint
-            ) as power_of_two_repr
-        from
-            curr
-            cross join unnest (sequence(date('2021-01-01'), date('2021-01-31'))) as t (sequence_day) --@start_date and @end_date of period we would like to represent history
+                    ELSE 0
+                END AS BIGINT
+            ) AS POWER_OF_TWO_REPR
+        FROM
+            CURR
+            CROSS JOIN UNNEST (SEQUENCE(DATE('2021-01-01'), DATE('2021-01-31'))) AS T (SEQUENCE_DAY) --@start_date and @end_date of period we would like to represent history
     )
-select
-    user_id,
-    browser_type,
-    to_base(sum(power_of_two_repr), 2) as binary_representation_dates_active
-from
-    conversion
-group by
-    user_id,
-    browser_type
+SELECT
+    USER_ID,
+    BROWSER_TYPE,
+    TO_BASE(SUM(POWER_OF_TWO_REPR), 2) AS BINARY_REPRESENTATION_DATES_ACTIVE
+FROM
+    CONVERSION
+GROUP BY
+    USER_ID,
+    BROWSER_TYPE
