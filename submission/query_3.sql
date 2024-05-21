bf39e90bbf0c5741fcec7b21534f926a7667d53e
mmarquez225 <63692729+mmarquez225@users.noreply.github.com>
2024-05-20 19:56:27 -0700

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,9 +1,13 @@
+-- Insert data into the user_devices_cumulated table
 INSERT INTO user_devices_cumulated
 SELECT
     we.user_id,
     d.browser_type,
-    ARRAY_AGG(we.event_date ORDER BY we.event_date DESC) as dates_active,
+    -- Aggregate event dates into an array, ordered by date in descending order
+    ARRAY_AGG(DATE(we.event_time) ORDER BY DATE(we.event_time) DESC) as dates_active,
     CURRENT_DATE as date
-FROM web_events we
-JOIN devices d ON we.device_id = d.device_id
+FROM bootcamp.web_events we
+-- Join web_events with devices to get browser_type
+JOIN bootcamp.devices d ON we.device_id = d.device_id
+-- Group by user_id and browser_type to get unique combinations
 GROUP BY we.user_id, d.browser_type
