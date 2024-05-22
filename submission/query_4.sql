7c72c4dc2bd09205428c2657438801764ce72540
hdamerla <harshithadamerla@gmail.com>
2024-05-21 19:25:38 -0500

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -7,7 +7,9 @@ With today AS ( select
     from
         hdamerla.user_devices_cumulated   
     where
-        date = DATE('2023-01-07') ), date_list_int AS ( select
+        date = DATE('2023-01-07') ),
+       
+     date_list_int AS ( select
         user_id,
         browser_type,
         CAST(SUM(CASE  --using bitwise operations calculating the integer representation
@@ -26,25 +28,9 @@ With today AS ( select
         user_id,
         browser_type )  
        
-        select
-        *,
-        TO_BASE(history_int,
-        2) as history_in_binary,
-        FROM_BASE('11111110000000000000000000000000',
-        2) AS weekly_base,
-        BIT_COUNT(history_int,
-        64) as num_days_active,
-        BIT_COUNT(BITWISE_AND(history_int,
-        FROM_BASE('11111110000000000000000000000000',
-        2)),
-        64) > 0 as is_weekly_active,
-        BIT_COUNT(BITWISE_AND(history_int,
-        FROM_BASE('00000001111111000000000000000000',
-        2)),
-        64) > 0 as is_weekly_active_last_week,
-        BIT_COUNT(BITWISE_AND(history_int,
-        FROM_BASE('11100000000000000000000000000000',
-        2)),
-        64) > 0 as is_active_last_three_days 
+    select
+        user_id,
+        browser_type,
+        TO_BASE(history_int, 2) as history_in_binary
     from
         date_list_int
