ecb378f0e63929169fb1c85e296508665d2de7ee
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2024-05-21 20:34:52 -0600

    Update query_8.sql
    Modifying query complete to pass the check

--- submission/query_8.sql
+++ submission/query_8.sql
@@ -15,27 +15,27 @@ WITH yesterday AS (
         FROM
             grisreyesrios.daily_web_metrics
         WHERE
-            DATE = DATE('2023-08-04')
+            DATE = DATE('2023-08-02')
     )
 
 SELECT
     COALESCE(
-        yest.host,
-        tod.host
+        y.host,
+        t.host
     ) AS host,
     COALESCE(
-        yest.metric_name,
-        tod.metric_name
+        y.metric_name,
+        t.metric_name
     ) AS metric_name,
     COALESCE(
-        yest.metric_array,
+        y.metric_array,
         REPEAT(
             NULL,
-            CAST(date_diff('day', DATE('2023-08-01'), tod.date) AS INTEGER))
-        ) || ARRAY [tod.metric_value] AS metric_array,
+            CAST(date_diff('day', DATE('2023-08-01'), t.date) AS INTEGER))
+        ) || ARRAY [t.metric_value] AS metric_array,
         '2023-08-01' AS month_start
         FROM
-            today tod full
-            OUTER JOIN yesterday yest
-            ON tod.host = yest.host
-            AND tod.metric_name = yest.metric_name
+            today t full
+            OUTER JOIN yesterday y
+            ON t.host = y.host
+            AND t.metric_name = y.metric_name
