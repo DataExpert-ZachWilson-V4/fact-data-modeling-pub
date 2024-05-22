0ef8a65a0ddacdd58cc0dd86b5da271fa4c59f52
bucketz-build <134549544+bucketz-build@users.noreply.github.com>
2024-05-22 00:48:21 -0400

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -2,7 +2,7 @@ WITH ranked_details AS (
     SELECT
         *,
         ROW_NUMBER() OVER (PARTITION BY game_id, team_id, player_id ORDER BY game_id) AS row_num
-    FROM nba_game_details
+    FROM bootcamp.nba_game_details
 )
 SELECT
     game_id,
