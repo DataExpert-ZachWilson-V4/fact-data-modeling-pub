a13ea082ed5396427b4c7f289d38e5955082ddba
RaniaAlzahrani <168978591+RaniaAlzahrani@users.noreply.github.com>
2024-05-22 09:52:55 +0300

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,4 +1,3 @@
--- Insert new data into the user_devices_cumulated table
 INSERT INTO raniasalzahrani.user_devices_cumulated
 WITH
     -- Retrieve all records from the user_devices_cumulated table for the previous day
@@ -8,7 +7,7 @@ WITH
         FROM
             raniasalzahrani.user_devices_cumulated
         WHERE
-            date = DATE('2023-01-04')
+            date = DATE('2022-01-04')
     ),
     -- Fetch current day activity from web_events and devices tables
     today AS (
@@ -16,12 +15,12 @@ WITH
             we.user_id,  -- Select user_id from web_events table
             de.browser_type,  -- Select browser_type from devices table
             CAST(date_trunc('day', we.event_time) AS DATE) AS event_date,  -- Convert event_time to event_date
-            DATE('2023-01-05') AS date  -- Set the date to 2023-01-05
+            DATE('2022-01-05') AS date  -- Set the date to 2022-01-05
         FROM
             bootcamp.web_events we
         INNER JOIN bootcamp.devices de ON we.device_id = de.device_id  -- Join web_events and devices tables on device_id
         WHERE
-            date_trunc('day', we.event_time) = DATE('2023-01-05')  -- Filter for events on 2023-01-05
+            date_trunc('day', we.event_time) = DATE('2022-01-05')  -- Filter for events on 2023-01-05
         GROUP BY
             we.user_id,
             de.browser_type,
@@ -37,7 +36,7 @@ SELECT
         WHEN y.dates_active IS NOT NULL THEN ARRAY[t.event_date] || y.dates_active
         ELSE ARRAY[t.event_date]
     END AS dates_active,
-    DATE('2023-01-05') AS date  -- Set the date to the current date
+    DATE('2022-01-05') AS date  -- Set the date to the current date
 FROM
     yesterday y
 FULL OUTER JOIN today t ON y.user_id = t.user_id AND y.browser_type = t.browser_type  -- Perform a full outer join on user_id and browser_type
