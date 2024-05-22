012ac27276b846118d5e3ba861d3e305348aab6a
Youkang Shon <ykshon527@gmail.com>
2024-05-21 22:11:02 -0500

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -1,3 +1,8 @@
+
+/*------------------------------------------------------------
+convert the date list implementation into the base-2 integer datelist representation
+*/------------------------------------------------------------
+
 WITH
   today AS (
     SELECT
@@ -11,6 +16,7 @@ WITH
     SELECT
       user_id,
       browser_type,
+    -- Sum those powers of 2 in a group by on user_id and browser_type
       CAST(SUM(CASE
                 WHEN CONTAINS(dates_active, sequence_date) THEN POW(2, 31 - DATE_DIFF('day', sequence_date, DATE))
                 ELSE 0
@@ -18,12 +24,15 @@ WITH
               ) AS BIGINT) AS history_int
     FROM
       today
+      --unnest the dates, and convert them into powers of 2
       CROSS JOIN UNNEST (SEQUENCE(DATE('2023-01-01'), DATE('2023-01-07'))) AS t (sequence_date)
     GROUP BY
       user_id, browser_type
   )
+  
 SELECT
   *,
+  --convert the sum to base 2
   TO_BASE(history_int, 2) AS history_in_binary
 FROM
   date_list_int
