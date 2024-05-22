83c7a8fbf650bdf0fe517e2b2b3206be5d3a4f38
Nitin Tharwani <tharwaninitin182@gmail.com>
2024-05-22 05:47:55 +0530

    Update query_8.sql

--- submission/query_8.sql
+++ submission/query_8.sql
@@ -10,12 +10,22 @@ WITH daily_web_metrics AS (
         END AS metric_name,
         COUNT(*) AS metric_value
     FROM bootcamp.web_events
-    GROUP BY 1,2,3
+    GROUP BY CAST(event_time AS DATE), host, CASE WHEN url = '/' THEN 'visited_home' WHEN url = '/blog' THEN 'visited_blog' ELSE 'visited_other' END
+),
+yesterday AS (
+    SELECT *
+    FROM tharwaninitin.host_activity_reduced
+    WHERE month_start = '2023-08-01'
+),
+today AS (
+    SELECT *
+    FROM daily_web_metrics
+    WHERE date = DATE('2023-08-02')
 )
-SELECT host,
-    metric_name,
-    ARRAY_AGG(metric_value) AS metric_array,
-    CAST(DATE_TRUNC('month', date) AS VARCHAR) AS month_start
-FROM daily_web_metrics
-GROUP BY 1,2,4
-ORDER BY 4,1,2
+SELECT
+    COALESCE(today.host, yesterday.host) AS host,
+    COALESCE(today.metric_name, yesterday.metric_name) AS metric_name,
+    COALESCE(yesterday.metric_array, REPEAT(NULL, CAST(DATE_DIFF('DAY', DATE('2023-08-01'), today.date) AS INTEGER))) || ARRAY[today.metric_value] AS metric_array,
+    '2023-08-01' AS month_start
+FROM today
+FULL OUTER JOIN yesterday ON today.host = yesterday.host AND today.metric_name = yesterday.metric_name
