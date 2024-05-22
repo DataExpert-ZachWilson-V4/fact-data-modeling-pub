64e4b20f5d29b319f766864ec91ae69bfdafbacd
Youkang Shon <ykshon527@gmail.com>
2024-05-21 19:52:34 -0500

    Update query_7.sql

---
+++ submission/query_7.sql
@@ -0,0 +1,11 @@
+CREATE TABLE ykshon52797255.host_activity_reduced (
+  host VARCHAR,
+  metric_name VARCHAR,
+  metric_array ARRAY(INTEGER),
+  month_start VARCHAR
+)
+WITH
+  (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['metric_name', 'month_start']
+  )
