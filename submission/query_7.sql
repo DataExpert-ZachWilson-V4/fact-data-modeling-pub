d52926cbd14b20b7c3cda5e7e45c7d4c57a9c28d
Tejal Kanase <136179334+TejalK14@users.noreply.github.com>
2024-05-22 00:29:14 -0400

    Update query_7.sql

---
+++ submission/query_7.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE TABLE host_activity_reduced
+(
+    host VARCHAR, 
+    metric_name VARCHAR,
+    metric_array ARRAY(INTEGER),
+    month_start VARCHAR
+)
+WITH
+(
+    format = 'PARQUET',
+    partitioning = ARRAY['metric_name', 'month_start']  
+)
