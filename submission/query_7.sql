3d8ef8251a51d58067894525be6acc9d69ada63d
Youkang Shon <ykshon527@gmail.com>
2024-05-21 20:05:08 -0500

    Update query_7.sql

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -1,4 +1,4 @@
-CREATE TABLE ykshon52797255.host_activity_reduced (
+CREATE OR REPLACE TABLE ykshon52797255.host_activity_reduced (
   host VARCHAR,
   metric_name VARCHAR,
   metric_array ARRAY(INTEGER),
