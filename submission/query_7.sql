aa48f0e032c8b570d49bed730069f9a1a7e047fd
Evan Brunt <47375706+evan-brunt@users.noreply.github.com>
2024-05-21 21:51:51 -0400

    Update query_7.sql

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE TABLE ebrunt.host_activity_reduced(
   host VARCHAR,
   metric_name VARCHAR,
   metric_array ARRAY(INTEGER),
-  month_start DATE
+  month_start VARCHAR
 )
 WITH (
   partitioning = ARRAY[ 'month_start' ]
