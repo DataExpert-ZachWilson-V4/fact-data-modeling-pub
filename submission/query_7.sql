c24d0b0759fdb61fb3d52b86f38ff1bcd66e0742
Youkang Shon <ykshon527@gmail.com>
2024-05-21 22:29:16 -0500

    Update query_7.sql

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -1,7 +1,15 @@
+/*-----------------------------------------------------
+create a table named host_activity_reduced that tracks
+host's activity monthly
+*/-----------------------------------------------------
 CREATE OR REPLACE TABLE ykshon52797255.host_activity_reduced (
+  --host: the host
   host VARCHAR,
+  -- metric_name: name of the metric
   metric_name VARCHAR,
+  -- metric_array: the measure of metrics in an array that is cumulated
   metric_array ARRAY(INTEGER),
+  -- month_start: the date of the month
   month_start VARCHAR
 )
 WITH
