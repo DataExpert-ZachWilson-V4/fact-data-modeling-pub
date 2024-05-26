259686ec9af329173acf3a580adcbe0ca7d83a8d
Andrea Montes <mamontesp@gmail.com>
2024-05-26 09:27:19 -0500

    Query 2

---
+++ submission/query_2.sql
@@ -0,0 +1,10 @@
+CREATE TABLE mamontesp.user_devices_cumulated (
+	  user_id BIGINT
+	, browser_type VARCHAR
+	, dates_active ARRAY(DATE)
+	, date DATE
+)
+WITH (
+	format = 'PARQUET'
+	, partitioning = ARRAY['date']
+)
