c3361dcfdf31666a3ed98e6c7665eef234948efb
rgindallas <richardgarth68@gmail.com>
2024-06-11 18:13:25 -0500

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,3 +1,9 @@
 CREATE OR REPLACE TABLE rgindallas.hosts_cumulated ( 
-  host varchar, host_activity_datelist array(date), date date ) 
-  WITH ( format = 'PARQUET', format_version = 2, partitioning = ARRAY['date'] )
+  host varchar, 
+  host_activity_datelist array(date), 
+  date date 
+) 
+WITH ( 
+  format = 'PARQUET',
+  partitioning = ARRAY['date'] 
+)
