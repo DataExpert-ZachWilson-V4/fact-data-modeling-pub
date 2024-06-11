b2bcc92158848d2842ba4fa70016c9741b8172e1
rgindallas <richardgarth68@gmail.com>
2024-06-11 18:29:19 -0500

    Update query_7.sql

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -1,9 +1,11 @@
 CREATE
-OR REPLACE TABLE rgindallas.host_activity_reduced (
+OR REPLACE TABLE host_activity_reduced (
   host varchar,
   metric_name varchar,
   metric_array array(integer),
   month_start varchar
 )
 WITH
-  (FORMAT = 'PARQUET')
+  (FORMAT = 'PARQUET',
+  partitioning = ARRAY['metric_name', 'month_start']
+)
