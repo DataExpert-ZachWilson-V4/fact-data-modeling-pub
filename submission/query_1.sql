608e28ab619c4422f0129195f107210a8b2a61f4
rgindallas <richardgarth68@gmail.com>
2024-06-12 01:37:22 -0500

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,2 +1,7 @@
-SELECT DISTINCT *
-FROM bootcamp.nba_game_details
+WITH cte as (SELECT
+  row_number() OVER (PARTITION BY GAME_ID,TEAM_ID, PLAYER_ID) AS ROW_ID,
+  *
+FROM
+  bootcamp.nba_game_details)
+select * from cte
+WHERE ROW_ID = 1
