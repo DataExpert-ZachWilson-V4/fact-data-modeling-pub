0a61a5011ae51f510ab61825c4739d4b12ea8835
rgindallas <richardgarth68@gmail.com>
2024-06-12 01:40:01 -0500

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,7 +1,19 @@
-WITH cte as (SELECT
-  row_number() OVER (PARTITION BY GAME_ID,TEAM_ID, PLAYER_ID) AS ROW_ID,
+-- Using a Common Table Expression (CTE) to create a temporary table named 'deduped'
+WITH cte AS (
+  -- Assigning a unique row number to each row within a partition of game_id, team_id, and player_id
+  SELECT
+    ROW_NUMBER() OVER (
+      PARTITION BY game_id, team_id, player_id
+    ) AS row_id,
+    *
+  FROM
+    bootcamp.nba_game_details
+)
+-- Selecting all columns from the CTE 'deduped' where row_id is 1
+-- This effectively removes duplicate entries based on game_id, team_id, and player_id
+SELECT
   *
 FROM
-  bootcamp.nba_game_details)
-select * from cte
-WHERE ROW_ID = 1
+  cte
+WHERE
+  row_id = 1
