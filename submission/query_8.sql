e971f956cb68e2ecbd2f920da89346ac7c1a051b
rgindallas <richardgarth68@gmail.com>
2024-06-14 15:31:26 -0500

    Update query_8.sql

--- submission/query_8.sql
+++ submission/query_8.sql
@@ -1,29 +1,48 @@
--- ## Reduced Host Fact Array Implementation (`query_8.sql`)
--- As shown in fact data modeling day 3 lab, write a query to incrementally populate the `host_activity_reduced` table from a `daily_web_metrics` table. 
--- Assume `daily_web_metrics` exists in your query. Don't worry about handling the overwrites or deletes for overlapping data.
--- Remember to leverage a full outer join, and to properly handle imputing empty values in the array for windows where a host gets a visit in the middle of the array time window.
--- Note: For this query, you will use the daily_web_metrics table, which you created in Week 2, Lab 3. 
--- Ind populate data into the daily_web_metrics table.
--- Ensure that this table is created in your schema, as you did in Week 2 Lab. Mention the schema name in your CTE/query during the submission.
-
-
-INSERT INTO rgindallas.host_activity_reduced
-WITH yesterday AS (
-SELECT *
-  FROM rgindallas.host_activity_reduced
-  WHERE month_start = '2023-08-01'
-)
-, today AS (
-SELECT *
-  FROM rgindallas.daily_web_metrics
-  WHERE date = DATE('2023-08-01')
-)
+INSERT INTO
+  rgindallas.host_activity_reduced
+WITH
+  yesterday AS (
+    SELECT
+      host,
+      metric_name,
+      metric_array,
+      month_start
+    FROM
+      rgindallas.host_activity_reduced
+    WHERE
+      month_start = DATE('2021-01-01')
+  ),
+  today AS (
+    SELECT
+      host,
+      metric_name,
+      CAST(SUM(metric_value) AS INTEGER) AS metric_value,
+      DATE_TRUNC('day', DATE) AS DATE,
+      DATE_TRUNC('month', DATE) AS month_start
+    FROM
+      rgindallas.daily_web_metrics
+    WHERE
+      DATE_TRUNC('day', DATE) = DATE('2021-01-02')
+    GROUP BY
+      1,
+      2,
+      4,
+      5
+  )
 SELECT
-  COALESCE(t.host, y.host) as host,
-  COALESCE(t.metric_name, y.metric_name) as metric_name,
-  COALESCE(y.metric_array, REPEAT(null, CAST(DATE_DIFF('day', DATE('2023-08-01'), t.date) AS INTEGER))) || ARRAY[t.metric_value] as metric_array,
-  '2023-08-01' AS month_start
+  COALESCE(y.host, t.host) AS host,
+  COALESCE(y.metric_name, t.metric_name) AS metric_name,
+  COALESCE(
+    y.metric_array,
+    REPEAT(
+      NULL,
+      CAST(
+        DATE_DIFF('day', DATE('2021-01-01'), t.date) AS INTEGER
+      )
+    )
+  ) || ARRAY[t.metric_value] AS metric_array,
+  t.month_start
 FROM
-today t
-FULL OUTER JOIN 
-  yesterday y ON t.host = y.host AND t.metric_name = y.metric_name
+  yesterday AS y
+  FULL OUTER JOIN today AS t ON y.host = t.host
+  AND y.metric_name = t.metric_name
