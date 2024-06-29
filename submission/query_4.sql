e1ede96dad3aa4820652c827ff5bedd189b0b3d0
Mashael <mashaelhalmarzooq@gmail.com>
2024-06-29 16:43:21 +0300

    multifixes

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -2,17 +2,18 @@ WITH today AS (
     -- Select data for the specified date
     SELECT *
     FROM malmarzooq80856.user_devices_cumulated
-    WHERE date = DATE('2022-12-02')
+    WHERE date = DATE '2022-12-02'
 ),
 generating_date_list AS (
     -- Generate a list of dates for each user and browser type
     SELECT 
         user_id,
         browser_type,
+        dates_active,
         sequence_date
     FROM today
     CROSS JOIN 
-        UNNEST(SEQUENCE(DATE('2022-12-03'), DATE('2022-12-31'))) 
+        UNNEST(SEQUENCE(DATE '2022-12-03', DATE '2022-12-31')) 
         AS t(sequence_date)
 ),
 convert_date_to_int AS (
@@ -24,7 +25,7 @@ convert_date_to_int AS (
             SUM(
                 CASE 
                     WHEN CONTAINS(dates_active, sequence_date) THEN 
-                        CAST(POW(2, 31 - DATE_DIFF('day', sequence_date, DATE('2022-12-31'))) AS BIGINT)
+                        CAST(POW(2, 31 - DATE_DIFF('day', sequence_date, DATE '2022-12-31')) AS BIGINT)
                     ELSE 0 
                 END
             ) AS BIGINT
@@ -33,10 +34,18 @@ convert_date_to_int AS (
     GROUP BY  
         user_id,
         browser_type
+),
+bit_count AS (
+    SELECT 
+        user_id,
+        browser_type,
+        history_as_int,
+        LENGTH(REPLACE(TO_BASE(history_as_int, 2), '0', '')) AS total_active_days
+    FROM convert_date_to_int
 )
 SELECT 
     user_id,
     browser_type,
     TO_BASE(history_as_int, 2) AS activation_history,
-    BIT_COUNT(history_as_int) AS total_active_days
-FROM convert_date_to_int
+    total_active_days
+FROM bit_count
--- submission/query_6.sql
+++ submission/query_6.sql
@@ -1,18 +1,18 @@
 INSERT INTO malmarzooq80856.hosts_cumulated
 WITH today AS (
--- Select host and event date for events occurring on 2023-01-01
+    -- Select host and event date for events occurring on 2023-01-01
     SELECT
         host,
         CAST(date_trunc('day', we.event_time) AS DATE) AS event_date
     FROM bootcamp.web_events we
-    WHERE DATE_TRUNC('day', we.event_time) = DATE('2023-01-01')
+    WHERE DATE_TRUNC('day', we.event_time) = DATE '2023-01-01'
     GROUP BY 1, 2
 ),
 yesterday AS (
--- Select data from hosts_cumulated for the previous day
+    -- Select data from hosts_cumulated for the previous day
     SELECT *
     FROM malmarzooq80856.hosts_cumulated
-    WHERE date = DATE('2022-12-31')
+    WHERE date = DATE '2022-12-31'
 )
 -- Combine today's events with yesterday's data
 SELECT
@@ -22,6 +22,6 @@ SELECT
             THEN ARRAY[t.event_date] || y.host_activity_datelist
         ELSE ARRAY[t.event_date]
     END AS host_activity_datelist,
-    COALESCE(y.date + 1, t.event_date) AS date
+    COALESCE(DATE_ADD('day', 1, y.date), t.event_date) AS date
 FROM yesterday y
-FULL OUTER JOIN today t ON y.host = t.host
+FULL OUTER JOIN today t ON y.host = t.host;
